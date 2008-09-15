a9ec8d4c5ef475f194a5e989a729021ff7bd2293
David Hayward <spidahman@gmail.com>
2008-09-15 23:57:52 +0000

    Bumpf to display the calendar view when adding a new holiday. This will probably change a lot before it's finished.

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -1,4 +1,23 @@
 class HolidaysController < ApplicationController
   before_filter :check_login
   before_filter :check_roles
+
+  def new
+    include_extra_stylesheet('new-calendar')
+    @today = current_date
+    @date = session[:calendar_date] || @today
+    @weekday = convert_week_day_number(@date.wday)
+    @monthstart = convert_week_day_number(@date.beginning_of_month.wday)
+    @monthdays = @date.end_of_month.day
+    @monthend = convert_week_day_number(@date.end_of_month.wday)
+    @prevmonth = @date - 1.month
+    @nextmonth = @date + 1.month
+    @dayindex = 1
+    @weekindex = 1
+  end
+
+  protected
+    def convert_week_day_number(wday)
+      wday > 0 ? wday : 7
+    end
 end
