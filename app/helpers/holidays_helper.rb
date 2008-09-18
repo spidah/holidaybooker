cd877e919e5979626db4b9842c73a8b5455a0f6b
David Hayward <spidahman@gmail.com>
2008-09-18 00:27:55 +0000

    * Changed the link_month helper to properly create a link tag.  * Added a replace_day helper to change the day value of the given date.  * Added a date_id helper to convert a date to a string id (format ddmmyyyy).

--- app/helpers/holidays_helper.rb
+++ app/helpers/holidays_helper.rb
@@ -1,5 +1,13 @@
 module HolidaysHelper
   def link_month(date, text)
-    text
+    link_to(text, change_month_holidays_path(:date => date), :id => date, :class => 'change-month')
+  end
+
+  def replace_day(date, day)
+    date.beginning_of_month + day.days - 1
+  end
+
+  def date_id(date)
+    date.strftime('%d%m%Y')
   end
 end
