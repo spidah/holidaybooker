e5d1c701284bc9d67d4cd6788720722695743880
David Hayward <spidahman@gmail.com>
2008-09-20 21:38:31 +0000

    Flesh out the Holiday model. Also count the number of working days, and set confirmed to false, before saving and creating.

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,4 +1,25 @@
 class Holiday < ActiveRecord::Base
+  belongs_to :user
+
+  before_create :set_confirmed
+  before_save :count_working_days
+
   named_scope :unconfirmed, :conditions => {:confirmed => false}
   named_scope :confirmed, :conditions => {:confirmed => true}
+
+  validates_presence_of :start_date, :message => 'Please select a start date.'
+  validates_presence_of :end_date, :message => 'Please select an end date.'
+  validates_presence_of :reason, :message => 'Please give a reason for your holiday.'
+
+  protected
+    def count_working_days
+      self[:total_days] = 0
+      self[:start_date].upto(self[:end_date]) { |date|
+        self[:total_days] += 1 if date.wday != 0 && date.wday != 6
+      }
+    end
+
+    def set_confirmed
+      self[:confirmed] = false
+    end
 end
