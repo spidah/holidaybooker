c32362a280765d56581e16020de423f9247651f2
David Hayward <spidahman@gmail.com>
2008-09-23 22:49:22 +0000

    Display the users holidays in the index action of the users controller.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def link_to_count(count, link_text, singular, path)
+    if count > 0
+      "#{count} " + link_to("#{link_text} #{count == 1 ? singular : singular.pluralize}", path)
+    end
+  end
 end
