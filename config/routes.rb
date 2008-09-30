d15e871360aa74662eb59c89946356c89fd74021
David Hayward <spidahman@gmail.com>
2008-09-30 22:24:55 +0000

    Added admin controllers for the users and departments.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ ActionController::Routing::Routes.draw do |map|
   map.resource          :session
   map.resources         :holidays,                :collection => { :change_month => :get, :confirmed => :get, :unconfirmed => :get }
 
+  map.namespace(:admin) do |admin|
+    admin.resources :users,                       :controller => 'admin_users'
+    admin.resources :departments,                 :controller => 'admin_departments'
+  end
+
   map.home              '',                       :controller => 'holidays',            :action => 'index'
   map.login             '/login',                 :controller => 'sessions',            :action => 'new'
   map.logout            '/logout',                :controller => 'sessions',            :action => 'destroy'
