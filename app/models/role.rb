52a485c7510f58109e9fa06981cf9c415f220b95
David Hayward <spidahman@gmail.com>
2008-10-03 13:02:38 +0000

    Simplify getting a role by adding a class method.

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,8 @@
 class Role < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_and_belongs_to_many :rights
+
+  def self.get(name)
+    find(:first, :conditions => {:name => name})
+  end
 end
