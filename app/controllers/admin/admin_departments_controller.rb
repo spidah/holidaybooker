f245e245be00b2fa9e4a163250669bdd1924bc9e
David Hayward <spidahman@gmail.com>
2008-10-06 23:02:04 +0000

    Allow the departments to be controller by the admins.

--- app/controllers/admin/admin_departments_controller.rb
+++ app/controllers/admin/admin_departments_controller.rb
@@ -1,4 +1,44 @@
 class Admin::AdminDepartmentsController < ApplicationController
   before_filter :check_login
   before_filter :check_roles
+
+  def index
+    @departments = Department.pagination(params[:page], params[:dir] ? 'DESC' : 'ASC')
+    redirect_to(new_admin_department_path) if @departments.size == 0 && Department.all.size == 0
+  end
+
+  def new
+    @department = Department.new
+  end
+
+  def create
+    @department = Department.new(params[:department])
+    @department.save!
+    redirect_to(admin_departments_path)
+  rescue
+    flash[:error] = @department.errors
+    render(:action => :new)
+  end
+
+  def edit
+    @department = Department.find(params[:id])
+  end
+
+  def update
+    @department = Department.find(params[:id])
+    @department.update_attributes(params[:department])
+    redirect_to(admin_departments_path)
+  rescue
+    flash[:error] = @department.errors
+    redirect_to(edit_admin_department_path(@department))
+  end
+
+  def destroy
+    @department = Department.find(params[:id])
+    @department.destroy
+    redirect_to(admin_departments_path)
+  rescue
+    flash[:error] = 'Unable to delete the selected department'
+    redirect_to(admin_departments_path)
+  end
 end
--- app/models/department.rb
+++ app/models/department.rb
@@ -1,4 +1,8 @@
 class Department < ActiveRecord::Base
   has_many :users
   has_many :heads, :class_name => 'User', :conditions => {:head => true}
+
+  def self.pagination(page, dir = 'ASC')
+    paginate(:page => page, :per_page => 50, :order => "name #{dir}")
+  end
 end
