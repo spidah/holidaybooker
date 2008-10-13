8efce263f8505170fa36093053839c4fa66961bf
David Hayward <spidahman@gmail.com>
2008-10-13 22:39:32 +0000

    Removed the :get for the submitted action in the holidays controller as it's now the index action. Re-added the home route that maps to the home action in the application controller. This will redirect the user to where they need to be.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resource          :session
-  map.resources         :holidays,                :collection => { :change_month => :get, :submitted => :get, :confirmed => :get, :unconfirmed => :get }
+  map.resources         :holidays,                :collection => { :change_month => :get, :confirmed => :get, :unconfirmed => :get }
   map.resources         :departments,             :member => { :change_month => :get }
 
   map.namespace(:admin) do |admin|
@@ -8,10 +8,10 @@ ActionController::Routing::Routes.draw do |map|
     admin.resources :departments,                 :controller => 'admin_departments'
   end
 
+  map.home              '',                       :controller => 'application',         :action => 'home'
   map.login             '/login',                 :controller => 'sessions',            :action => 'new'
   map.logout            '/logout',                :controller => 'sessions',            :action => 'destroy'
   map.signup            '/signup',                :controller => 'sessions',            :action => 'signup'
 
-  map.connect           '',                       :controller => 'holidays',            :action => 'index'
   map.connect           '*path',                  :controller => 'holidays',            :action => 'index'
 end
