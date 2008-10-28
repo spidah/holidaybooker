0c743860bc70a3d2378bf3c70716b2145fa309c8
David Hayward <spidahman@gmail.com>
2008-10-28 20:18:34 +0000

    Added the sort_link helper method as we're not including all the helpers.

--- app/helpers/admin/admin_departments_helper.rb
+++ app/helpers/admin/admin_departments_helper.rb
@@ -1,2 +1,7 @@
 module Admin::AdminDepartmentsHelper
+  def sort_link(title, action = nil)
+    action = title.downcase if action == nil
+    direction = action == params[:sort] ? (params[:dir] == 'down' ? nil : 'down') : nil
+    link_to(title, admin_users_path(:sort => action, :dir => direction), :class => 'sort-header')
+  end
 end
