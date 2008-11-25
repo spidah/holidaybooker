0085d3a58c5eee297b79ac76e0d81fcc40695164
David Hayward <spidahman@gmail.com>
2008-11-25 00:09:46 +0000

    Need to do a custom sql find to get the department heads now that the users head column has been removed. Make is a bit more complex to read, but it's still better without the duplicated head value being stores.

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,6 +1,10 @@
 class Department < ActiveRecord::Base
   has_many :users
-  has_many :heads, :class_name => 'User', :conditions => {:head => true}
+  has_many :heads, :class_name => 'User', :finder_sql => %q(
+    SELECT * FROM users u, roles_users ru 
+    WHERE u.department_id = #{id} AND ru.user_id = u.id AND ru.role_id = 
+    (SELECT id FROM roles WHERE name = 'head')
+  )
 
   def self.pagination(page, dir = 'ASC')
     paginate(:page => page, :per_page => 50, :order => "name #{dir}")
